c55e00a2e0f1b50d56cb5ecd9f19a682ffa4f994
Knivemare <35490770+Knivemare@users.noreply.github.com>
2024-01-14 21:35:08 +0100

    Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import AppNavbar from '../components/AppNavbar'
 import AppFooter from '../components/AppFooter'
 
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 
 // Import Styles
 import styles from 'styles/App.module.scss'
@@ -17,6 +18,7 @@ function MyApp({ Component, pageProps }) {
       <main className={styles.Content}>
         <Component {...pageProps} />
         <SpeedInsights />
+        <Analytics />
       </main>
       <AppFooter/>
     </>
